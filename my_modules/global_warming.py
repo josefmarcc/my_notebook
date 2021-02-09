8cfb1e760267729e00f2d47b1d00cd364b72dcbe
josefmarcc <56867995+josefmarcc@users.noreply.github.com>
2021-02-09 18:59:31 +0100

    Added files for week 2

---
+++ my_modules/global_warming.py
@@ -0,0 +1,7 @@
+def global_warming_status():
+    return 'The globe is heating up!'
+
+    # Programmets main metode:
+if __name__ == '__main__':
+    status = global_warming_status()
+    print(status)
---
+++ my_modules/test_utils.py
@@ -0,0 +1,4 @@
+import utils as utils
+
+
+utils.get_file_names("/testPython.csv")
